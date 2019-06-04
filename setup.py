764a843ae5ee9d385b9657fdd9489a23a14701e5
Ryan Petrello <lists@ryanpetrello.com>
2019-06-04 11:16:48 -0400

    version increment

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import unittest
 
 setup(
     name='sdb',
-    version='1.5',
+    version='1.6',
     author='Ryan Petrello',
     author_email='ryan@ryanpetrello.com',
     url='https://github.com/ryanpetrello/sdb',
